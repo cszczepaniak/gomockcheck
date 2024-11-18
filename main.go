91f9af26eeb50bffecb5bc92ea51a4bb4e24842d
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-11-17 22:09:41 -0600

    add it to main

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/cszczepaniak/gomockcheck/analyzers/assertexpectations"
+	"github.com/cszczepaniak/gomockcheck/analyzers/mocksetup"
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
 func main() {
 	multichecker.Main(
 		assertexpectations.New(),
+		mocksetup.New(),
 	)
 }
